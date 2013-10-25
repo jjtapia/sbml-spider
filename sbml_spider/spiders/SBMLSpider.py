3e097b74c9b4a316f0a037125321cd38ea6b5020
Jose Juan Tapia <jjtapia@gmail.com>
2013-10-25 19:08:10 -0400

    adding randomized seed searching

--- sbml_spider/spiders/SBMLSpider.py
+++ sbml_spider/spiders/SBMLSpider.py
@@ -10,6 +10,7 @@ from scrapy.contrib.spiders import  CrawlSpider,Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.http import Request
 from sbml_spider.items import SbmlSpiderItem
+import random
 
 class SbmlSpider(CrawlSpider):
     name = 'sbml'
@@ -24,12 +25,12 @@ class SbmlSpider(CrawlSpider):
         self.jumps = 0
         self.maxJumps = 1000000
         self.alreadyVisited = []
-        self.start_urls = self.urls
+        rint = random.randint(0,len(self.urls)-1)
+        self.start_urls = self.urls[rint]
         self.rules = (
         # Extract links matching 'category.php' (but not matching 'subsection.php')
         # and follow links from them (since no callback means follow=True by default).
         Rule(SgmlLinkExtractor(allow=('\.xml', )),callback='parseXML')
-
         # Extract links matching 'item.php' and parse them with the spider's method parse_item
     )
 
@@ -37,10 +38,10 @@ class SbmlSpider(CrawlSpider):
     def parse(self,response):
         hxs = HtmlXPathSelector(response)
         for url in hxs.select('//a/@href').extract():
-            if 'xml' in url:
+            if '.xml' in url:
                 yield Request(url,callback=self.parseXML)
             elif 'http' in url or 'www' in url and url not in self.alreadyVisited:
-                self.alreadyVisited.append(url)
+                #self.alreadyVisited.append(url)
                 #print len(self.alreadyVisited),url
                 yield Request(url, callback=self.parse)
 
