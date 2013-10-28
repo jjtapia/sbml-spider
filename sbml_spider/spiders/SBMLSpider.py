409c864aed670d8d420e0298d118a8e25b21295e
Jose Juan Tapia <jjtapia@gmail.com>
2013-10-28 12:56:50 -0400

    added list of deny domains

--- sbml_spider/spiders/SBMLSpider.py
+++ sbml_spider/spiders/SBMLSpider.py
@@ -21,16 +21,24 @@ class SbmlSpider(CrawlSpider):
             tmp = json.load(f)
         self.urls = [x['Url'] for x in tmp]
         self.allowed_domains = []
+        self.deny_domains= ["www.youtube.com","www.facebook.com",
+                            "www.reddit.com","www.twitter.com","www.ebay.com","pages.ebay.com",
+                            "www.today.com","www.google.com","www.bing.com","www.yahoo.com",
+                            "www.apple.com","www.microsoft.com","twitter.com","windows.microsoft.com"]
+        self.deny= ["youtube","facebook","reddit","twitter","ebay","today","www.google.com","bing","yahoo",
+                    "apple","microsoft","twitter","flickr","digg","deviantart","tumblr","slashdot",
+                    "linkedin","sony","playstation","amazon","instagram"  ]
         self.counter = 0
         self.jumps = 0
         self.maxJumps = 1000000
         self.alreadyVisited = []
         rint = random.randint(0,len(self.urls)-1)
-        self.start_urls = self.urls[rint]
+        self.start_urls = [rint]
         self.rules = (
         # Extract links matching 'category.php' (but not matching 'subsection.php')
         # and follow links from them (since no callback means follow=True by default).
         Rule(SgmlLinkExtractor(allow=('\.xml', )),callback='parseXML')
+        
         # Extract links matching 'item.php' and parse them with the spider's method parse_item
     )
 
@@ -40,16 +48,19 @@ class SbmlSpider(CrawlSpider):
         for url in hxs.select('//a/@href').extract():
             if '.xml' in url:
                 yield Request(url,callback=self.parseXML)
-            elif 'http' in url or 'www' in url and url not in self.alreadyVisited:
+            elif ('http' in url or 'www' in url) and url not in self.alreadyVisited and \
+            len([x for x in self.deny if x in str(url)]) == 0:
                 #self.alreadyVisited.append(url)
                 #print len(self.alreadyVisited),url
                 yield Request(url, callback=self.parse)
 
     def parseXML(self,response):
         hxs = HtmlXPathSelector(response)
+        
         print 'sbml' in hxs.extract_unquoted(),self.counter
         if 'sbml' in hxs.extract_unquoted():
-            f =  open('results{0}.xml'.format(random.randint(0,1000000)),'w')
+            fileName = response.url.split("/")[-2]
+            f =  open('results_{0}.xml'.format(fileName),'w')
             f.write(hxs.extract_unquoted())
             f.flush()
             self.counter += 1
