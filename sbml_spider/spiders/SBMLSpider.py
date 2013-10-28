e54f7647b309c632461f5578905a6f0b277e3b61
Jose Juan Tapia <jjtapia@gmail.com>
2013-10-28 13:00:01 -0400

    correcting mistakes

--- sbml_spider/spiders/SBMLSpider.py
+++ sbml_spider/spiders/SBMLSpider.py
@@ -33,7 +33,8 @@ class SbmlSpider(CrawlSpider):
         self.maxJumps = 1000000
         self.alreadyVisited = []
         rint = random.randint(0,len(self.urls)-1)
-        self.start_urls = [rint]
+        print self.urls[rint]
+        self.start_urls = [self.urls[rint]]
         self.rules = (
         # Extract links matching 'category.php' (but not matching 'subsection.php')
         # and follow links from them (since no callback means follow=True by default).
